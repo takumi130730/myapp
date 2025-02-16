509e3bdfc283947ef2eae3ab906ef5afd554bff2
takumi130730 <takumi130730.business@gmail.com>
2025-02-16 23:04:10 +0900

    一覧画面にヘッダーとナビゲーション追加

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import './App.css';
+import { useState } from 'react';
+import List from './List';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setIsLoggedIn(true);
+  };
+
   return (
     <div className="formContainer">
-      <form>
-        <h1>ログインフォーム</h1>
-        <hr />
-        <div class="uiForm">
-          <div class="formField">
-            <lavel>ユーザ名</lavel>
-            <input type="text" placeholder='ユーザ名' name="username" />
+      {isLoggedIn ? (
+        <List />
+      ) : (
+        <form onSubmit={handleLogin}>
+          <h1>ログインフォーム</h1>
+          <hr />
+          <div class="uiForm">
+            <div class="formField">
+              <lavel>ユーザ名</lavel>
+              <input type="text" placeholder='ユーザ名' name="username" />
+            </div>
+            <div class="formField">
+              <lavel>メールアドレス</lavel>
+              <input type="text" placeholder='メールアドレス' name="mailAddress" />
+            </div>
+            <div class="formField">
+              <lavel>パスワード</lavel>
+              <input type="text" placeholder='パスワード' name="password" />
+            </div>
+            <button type='submit' name='submitButton'>ログイン</button>
           </div>
-          <div class="formField">
-            <lavel>メールアドレス</lavel>
-            <input type="text" placeholder='メールアドレス' name="mailAddress" />
-          </div>
-          <div class="formField">
-            <lavel>パスワード</lavel>
-            <input type="text" placeholder='パスワード' name="password" />
-          </div>
-          <button name='submitButton'>ログイン</button>
-        </div>
-      </form>
+        </form>
+      )}
     </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './List.css';
 
 function List() {
   const items = ['item 1', 'item 2', 'item 3'];
 
   return (
     <div className="listContainer">
-      <h2>一覧画面</h2>
+      <header className="listHeader">
+        <h1 className="headerTitle">一覧画面</h1>
+      </header>
+      <nav className="listNav">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
       <ul>
         {items.map((item, index) => (
           <li key={index}>{item}</li>
